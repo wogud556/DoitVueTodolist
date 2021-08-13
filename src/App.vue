7ead17b314de5a6f363c8caa2deeed1a0434c5b6
wogud556 <wogud556@naver.com>
2021-08-13 15:31:12 +0900

    고도화 작업

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,9 @@
 <template>
   <div id="app">
     <TodoHeader></TodoHeader>
-    <TodoInput></TodoInput>
-    <TodoList></TodoList>
-    <TodoFooter></TodoFooter>
+    <TodoInput v-on:addTodo="addTodo"></TodoInput>
+    <TodoList v-bind:propsdata="todoItems" @removeTodo="removeTodo"></TodoList>
+    <TodoFooter v-on:removeAll="clearAll"></TodoFooter>
   </div>
 </template>
 
@@ -14,6 +14,33 @@ import TodoList from './conponents/TodoList.vue'
 import TodoFooter from './conponents/TodoFooter.vue'
 
 export default {
+  data(){
+    return{
+      todoItems:[]
+    }
+  },
+  created(){
+    if(localStorage.length>0){
+        for(var i = 0; i< localStorage.length; i++){
+            this.todoItems.push(localStorage.key(i));
+        }
+    }
+  },
+  methods:{
+    clearAll(){
+      localStorage.clear();
+      this.todoItems = [];
+    },
+    addTodo(todoItem){
+      localStorage.setItem(todoItem, todoItem);
+      this.todoItems.push(todoItem);
+    },
+    removeTodo(todoItem, index){
+      localStorage.removeItem(todoItem);
+      this.todoItems.splice(index,1);
+    }
+
+},
   components:{
     'TodoHeader' : TodoHeader,
     'TodoInput' : TodoInput,
--- src/conponents/TodoInput.vue
+++ src/conponents/TodoInput.vue
@@ -1,30 +1,46 @@
 <template>
     <div class="inputBox shadow">
         <input type="text" v-model = "newTodoItem" placeholder = "Type what you have to do" v-on:keyup.enter = "addTodo">
-        <span class = "addContainer" v-on:click = "addTodo" style="vertical-align : middle;">
+        <span class = "addContainer" v-on:click = "addTodo" >
             <i class="addBtn fas fa-plus" aria-hidden="true"></i>
         </span>
+        <modal v-if="showModal" @close="showModal = false">
+            <h3 slot="header">경고</h3>
+            <span slot="footer" @click="showModal = false">
+                할 일을 입력하세요.
+                <i class="closeModalBtn fas fa-times" aria-hidden="true"></i>
+            </span>
+        </modal>
     </div>
 </template>
 
 <script>
+import Modal from './common/Modal.vue'
+
 export default {
+    props:['propsdata'],
     data(){
         return {
-            newTodoItem:''
+            newTodoItem:'',
+            showModal: false
         }
     },
     methods:{
         addTodo(){
             if(this.newTodoItem !== ""){
                 var value = this.newTodoItem && this.newTodoItem.trim();
-                localStorage.setItem(value, value);
+                this.$emit('addTodo', value);
                 this.clearInput();
+            }else{
+                this.showModal = !this.showModal;
             }
         },
         clearInput(){
             this.newTodoItem = "";
         }
+    },
+    components:{
+        Modal: Modal
     }
 }
 </script>
